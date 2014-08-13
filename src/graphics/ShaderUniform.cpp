f165c231b9d4f96773dd60f724ddfedfc9e9f9aa
George Burton <burtonageo@gmail.com>
2014-08-13 15:00:17 +0100

    Added ShaderUniform class

---
+++ src/graphics/ShaderUniform.cpp
@@ -0,0 +1,52 @@
+/**
+ * This file is part of the pneumatic game engine
+ *
+ * Copyright (c) 2014 George Burton
+ * 
+ * THIS SOFTWARE IS PROVIDED 'AS-IS', WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY. IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES
+ * ARISING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,  
+ * including commercial applications, and to alter it and redistribute it  
+ * freely, subject to the following restrictions:
+ * 
+ *    1. The origin of this software must not be misrepresented; you must not  
+ *       claim that you wrote the original software. If you use this software  
+ *       in a product, an acknowledgment in the product documentation would be  
+ *       appreciated but is not required.
+ * 
+ *    2. Altered source versions must be plainly marked as such, and must not be  
+ *       misrepresented as being the original software.
+ * 
+ *    3. This notice may not be removed or altered from any source  
+ *       distribution.
+ *
+ **/
+
+#include "pneu/graphics/ShaderUniform.hpp"
+#include "pneu/graphics/Shader.hpp"
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+#define GLFW_INCLUDE_GL3
+#define GLFW_NO_GLU
+#include <GLFW/glfw3.h>
+
+#define DECLARE_SHADERUNIFORM(type, update_function) \
+  template<> \
+  auto \
+  pneu::graphics::ShaderUniform<type>::update(const type& uniform_value) -> void  \
+  { \
+    ShaderId variable_id = fShader->getShaderProgram(); \
+    auto variable_ref = glGetUniformLocation(variable_id, fVariableName.c_str()); \
+    update_function; \
+  }
+
+//DECLARE_SIMPLE_SHADERUNIFORM(bool,  glUniform1b)
+DECLARE_SHADERUNIFORM(float,     glUniform1f(variable_ref, uniform_value))
+DECLARE_SHADERUNIFORM(glm::mat4, glUniformMatrix4fv(variable_ref, 1, GL_FALSE, &uniform_value[0][0]))

d3ab51f27f1bf1d16c27562dba78459e1c828e54
George Burton <burtonageo@gmail.com>
2014-08-13 11:41:53 +0100

    Added angle types

---
+++ include/pneu/core/Angle.hpp
@@ -0,0 +1,121 @@
+/**
+ * This file is part of the pneumatic game engine
+ *
+ * Copyright (c) 2014 George Burton
+ * 
+ * THIS SOFTWARE IS PROVIDED 'AS-IS', WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY. IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES
+ * ARISING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,  
+ * including commercial applications, and to alter it and redistribute it  
+ * freely, subject to the following restrictions:
+ * 
+ *    1. The origin of this software must not be misrepresented; you must not  
+ *       claim that you wrote the original software. If you use this software  
+ *       in a product, an acknowledgment in the product documentation would be  
+ *       appreciated but is not required.
+ * 
+ *    2. Altered source versions must be plainly marked as such, and must not be  
+ *       misrepresented as being the original software.
+ * 
+ *    3. This notice may not be removed or altered from any source  
+ *       distribution.
+ *
+ **/
+
+#pragma once
+
+#ifndef PNEUMATIC_MATHUTIL_HPP
+#define PNEUMATIC_MATHUTIL_HPP
+
+template<typename T>
+struct Radians;
+
+template<typename T>
+struct Degrees;
+
+template<typename T>
+struct Radians {
+public:
+  explicit Radians(const T& val)
+    :
+    fValue(val) { }
+
+  inline auto toDegrees() -> Degrees<T>
+  {
+    return Degrees<T>(fValue * static_cast<T>(57.629));
+  }
+
+  inline auto value() -> T
+  {
+    return fValue;
+  }
+/*
+  auto operator+ (const Radians<T>& other) -> Radians<T>
+  {
+    return Radians(value + other.value);
+  }
+
+  auto operator- (const Radians<T>& other) -> Radians<T>
+  {
+    return Radians(value - other.value);
+  }
+
+  auto operator* (const Radians<T>& other) -> Radians<T>
+  {
+    return Radians(value * other.value);
+  }
+
+  auto operator/ (const Radians<T>& other) -> Radians<T>
+  {
+    return Radians(value / other.value);
+  }
+*/
+
+private:
+  T fValue;
+};
+
+template<typename T>
+struct Degrees {
+public:
+  explicit Degrees(const T& val)
+      :
+      fValue(val) { }
+
+  inline auto toRadians() -> Radians<T>
+  {
+    return Radians<T>(fValue * static_cast<T>(0.017));
+  }
+
+  inline auto value() -> T
+  {
+    return fValue;
+  }
+/*
+  auto operator+ (const Degrees<T>& other) -> Degrees<T>
+  {
+    return Degrees(value + other.value);
+  }
+
+  auto operator- (const Degrees<T>& other) -> Degrees<T>
+  {
+    return Degrees(value - other.value);
+  }
+
+  auto operator* (const Degrees<T>& other) -> Degrees<T>
+  {
+    return Degrees(value * other.value);
+  }
+
+  auto operator/ (const Degrees<T>& other) -> Degrees<T>
+  {
+    return Degrees(value / other.value);
+  }
+*/
+private:
+  T fValue;
+};
+
+#endif // PNEUMATIC_MATHUTIL_HPP

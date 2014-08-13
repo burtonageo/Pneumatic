5661cc457076d01218c977dc0e36d49e12acee29
George Burton <burtonageo@gmail.com>
2014-08-13 11:39:33 +0100

    Initial color class defined

---
+++ include/pneu/graphics/Color.hpp
@@ -0,0 +1,129 @@
+/**
+ * This file is part of the pneu game engine
+ *
+ * Copyright (c) 2014 George Burton
+ * 
+ * THIS SOFTWARE IS PROVIDED 'AS-IS', WITHOUT ANY EXPRESS OR IMPLIED
+ * WARRANTY. IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES
+ * ARISING FROM THE USE OF THIS SOFTWARE.
+ * 
+ * Permission is granted to anyone to use this software for any purpose,  
+ * including commercial applications, and to alter it and redistribute it  
+ * freely, subject to the following restrictions:
+ * 
+ *    1. The origin of this software must not be misrepresented; you must not  
+ *       claim that you wrote the original software. If you use this software  
+ *       in a product, an acknowledgment in the product documentation would be  
+ *       appreciated but is not required.
+ * 
+ *    2. Altered source versions must be plainly marked as such, and must not be  
+ *       misrepresented as being the original software.
+ * 
+ *    3. This notice may not be removed or altered from any source  
+ *       distribution.
+ *
+ **/
+
+#pragma once
+
+#ifndef PNEUMATIC_COLOR_HPP
+#define PNEUMATIC_COLOR_HPP
+
+#define GLM_FORCE_RADIANS
+#include <glm/glm.hpp>
+
+template<typename T>
+class Color4t;
+
+template<typename T>
+class Color3t {
+public:
+  Color3t(T r, T g, T b)
+    :
+    fColVec(r, g, b) { }
+
+  auto toVector() -> glm::tvec3
+  {
+    return fColVec;
+  }
+
+  auto toColor4(T alpha = static_cast<T>(1.0)) const -> Color4t;
+
+  inline auto r() -> T
+  {
+    return fColVec.r;
+  }
+
+  inline auto g() -> T
+  {
+    return fColVec.g;
+  }
+
+  inline auto b() -> T
+  {
+    return fColVec.b;
+  }
+
+private:
+  glm::tvec3<T> fColVec;
+}
+
+template<typename T>
+class Color4t {
+public:
+  Color4t(T r, T g, T b, T a)
+    :
+    fColVec(r, g, b, a) { }
+
+  auto toVector() const -> glm::tvec4
+  {
+    return fColVec;
+  }
+
+  auto toColor3() const -> Color3t;
+
+  
+  inline auto r() -> T
+  {
+    return fColVec.r;
+  }
+  
+  inline auto g() -> T
+  {
+    return fColVec.g;
+  }
+  
+  inline auto b() -> T
+  {
+    return fColVec.b;
+  }
+
+  inline auto a() -> T
+  {
+    return fColVec.a;
+  }
+
+private:
+  glm::vec4<T> fColVec;
+}
+
+template<typename T>
+auto
+pneu::graphics::Color3::toColor4(T alpha) const -> Color4t
+{
+  return Color4t<T>(fColVec.r, fColVec.g, fColVec.b, alpha);
+}
+
+template<typename T>
+auto
+pneu::graphics::Color4::toColor3() const -> Color3t
+{
+  return Color4t<T>(fColVec.r, fColVec.g, fColVec.b);
+}
+
+typedef Color3  Color3t<float>;
+typedef Color3d Color3t<double>;
+typedef Color4  Color4t<float>;
+typedef Color4d Color4t<double>;
+
+#endif // PNEUMATIC_COLOR_HPP

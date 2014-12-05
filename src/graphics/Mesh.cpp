bdef76ae016a566cdacecc123712074c58f7e318
George Burton <burtonageo@gmail.com>
2014-12-05 22:26:38 +0000

    Disable warnings for dependency headers

--- src/graphics/Mesh.cpp
+++ src/graphics/Mesh.cpp
@@ -2,24 +2,24 @@
  * This file is part of the pneumatic game engine
  *
  * Copyright (c) 2014 George Burton
- * 
+ *
  * THIS SOFTWARE IS PROVIDED 'AS-IS', WITHOUT ANY EXPRESS OR IMPLIED
  * WARRANTY. IN NO EVENT WILL THE AUTHORS BE HELD LIABLE FOR ANY DAMAGES
  * ARISING FROM THE USE OF THIS SOFTWARE.
- * 
- * Permission is granted to anyone to use this software for any purpose,  
- * including commercial applications, and to alter it and redistribute it  
+ *
+ * Permission is granted to anyone to use this software for any purpose,
+ * including commercial applications, and to alter it and redistribute it
  * freely, subject to the following restrictions:
- * 
- *    1. The origin of this software must not be misrepresented; you must not  
- *       claim that you wrote the original software. If you use this software  
- *       in a product, an acknowledgment in the product documentation would be  
+ *
+ *    1. The origin of this software must not be misrepresented; you must not
+ *       claim that you wrote the original software. If you use this software
+ *       in a product, an acknowledgment in the product documentation would be
  *       appreciated but is not required.
- * 
- *    2. Altered source versions must be plainly marked as such, and must not be  
+ *
+ *    2. Altered source versions must be plainly marked as such, and must not be
  *       misrepresented as being the original software.
- * 
- *    3. This notice may not be removed or altered from any source  
+ *
+ *    3. This notice may not be removed or altered from any source
  *       distribution.
  *
  **/
@@ -30,12 +30,13 @@
 #include <iostream>
 #include <fstream>
 
-#define GLEW_STATIC
-#include <GL/glew.h>
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdocumentation"
 
-#define GLFW_INCLUDE_GL3
-#define GLFW_NO_GLU
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+#pragma clang diagnostic pop
 
 #include "pneu/core/ResourceLoader.hpp"
 #include "pneu/graphics/Shader.hpp"
